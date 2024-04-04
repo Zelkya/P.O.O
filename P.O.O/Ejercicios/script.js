ddfa819aab7643c8effb9f8b23333396b5305290
Zelkya <tomasgonzalezg@liceovvh.cl>
2024-04-04 11:53:06 -0300

    New version

---
+++ P.O.O/Ejercicios/script.js
@@ -0,0 +1,12 @@
+console.log("Vinculando esperate un cachito wn......");
+
+function IdentificarMayorEdad(){
+
+    let edad = prompt("Por favor ingrese su edad mae: ");
+    
+    if(edad >= 18){
+        alert(`Es mayor de edad: ${edad}`);
+    } else {
+        alert("Es menor de edad:");
+    }
+};

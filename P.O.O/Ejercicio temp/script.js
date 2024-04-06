1753e7e91ba8de289d99da8bff69192e30ea62a3
Tomás González <163433071+Zelkya@users.noreply.github.com>
2024-04-06 20:39:56 -0300

    new

---
+++ P.O.O/Ejercicio temp/script.js
@@ -0,0 +1,18 @@
+console.log("Wait a minute please!");
+
+function temperaturas() {
+  let temp = 0;
+  let temp_ask = prompt("Por favor ingrese su temperatura actual");
+
+  if (temp_ask < 0) {
+    temp = `Hace frío wachito <h2>${temp_ask}</h2>`;
+  } else if (temp_ask >= 0 && temp_ask <= 25) {
+    temp = `La temperatura es agradable <h2>${temp_ask}</h2>`;
+  } else if (temp_ask > 25) {
+    temp = `Hace calor <h2>${temp_ask}</h2>`;
+  }
+
+  document.getElementById("lista").innerHTML = temp;
+};
+
+
